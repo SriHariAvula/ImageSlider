73338f6f6249c04f9b6d34ae845bde06156554c1
SriHariAvula <avulasrihari7292@gmail.com>
2025-04-08 14:41:23 -0500

    Add functionality to the slides

---
+++ script.js
@@ -0,0 +1,22 @@
+let currentSlide = 0;
+let slides = document.querySelectorAll(".slide");
+let prevBtn = document.querySelector(".prev");
+let nextBtn = document.querySelector(".next");
+
+function next() {
+  let nextSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].style.display = "none";
+  slides[nextSlide].style.display = "block";
+  currentSlide = nextSlide;
+}
+
+nextBtn.addEventListener("click", next);
+
+prevBtn.addEventListener("click", function () {
+  let prevSlide = currentSlide == 0 ? slides.length - 1 : currentSlide - 1;
+  slides[currentSlide].style.display = "none";
+  slides[prevSlide].style.display = "block";
+  currentSlide = prevSlide;
+});
+
+setInterval(next, 5000);
